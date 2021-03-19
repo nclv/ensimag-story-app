b9badcb9fdc8bd210c6a0b13944ed6feb37276fa
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2021-03-19 10:28:41 +0100

    Path contient les différentes URLs.

---
+++ src/main/java/utils/Path.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * Classe statique contenant les différents chemins des vues.
+ */
+public final class Path {
+    private Path() {}
+
+    public static final String PAGE_ERROR = "jsp/error.jsp";
+    public static final String PAGE_HOME = "jsp/home.jsp";
+    public static final String PAGE_ABOUT = "jsp/about.jsp";
+    public static final String PAGE_LOGIN = "jsp/login.jsp";
+
+    public static final String REDIRECT_HOME = "/controller?action=home";
+}
