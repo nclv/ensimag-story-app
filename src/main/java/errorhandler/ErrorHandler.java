5810e23923e5302f9d3e6ddaa76042be95938079
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2021-03-21 16:32:39 +0100

    Gestion des codes d'erreurs, fonctionnel si error-page de web.xml est décommenté.

--- src/main/java/errorhandler/ErrorHandler.java
+++ src/main/java/errorhandler/ErrorHandler.java
@@ -1,5 +1,30 @@
 package errorhandler;
 
-public class ErrorHandler {
-    
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import utils.Path;
+
+@WebServlet("/errorhandler")
+public class ErrorHandler extends HttpServlet {
+
+    private static final long serialVersionUID = -8097261662780128974L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processError(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processError(req, resp);
+    }
+
+    private void processError(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect(Path.PAGE_ERROR);
+    }
 }
