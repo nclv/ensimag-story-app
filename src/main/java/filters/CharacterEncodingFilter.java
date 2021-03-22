a0060c9ae8b0d0862f91a1a49fae2df1ee0dc858
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2021-03-22 11:24:30 +0100

    Character encoding filter

---
+++ src/main/java/filters/CharacterEncodingFilter.java
@@ -0,0 +1,30 @@
+package filters;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class CharacterEncodingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
