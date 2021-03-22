8720e8f27ef6a433c2d318ee25ac5023ca361133
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2021-03-22 14:51:15 +0100

    Story and Paragraphe fields

--- src/main/java/models/DatabaseFields.java
+++ src/main/java/models/DatabaseFields.java
@@ -7,4 +7,12 @@ public final class DatabaseFields {
     public static final String USER_ID = "user_id";
     public static final String USER_USERNAME = "username";
     public static final String USER_PASSWORD = "password";
+
+    public static final String STORY_ID = "story_id";
+    public static final String STORY_OPEN = "open";
+    public static final String STORY_PUBLISHED = "published";
+
+    public static final String PARAGRAPHE_ID = "para_id";
+    public static final String PARAGRAPHE_CONTENT = "content";
+    public static final String PARAGRAPHE_IS_FINAL = "is_final";
 }
