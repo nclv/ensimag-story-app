ec1ceb569468110a4ae424f6aad0857201760d9b
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2021-03-25 11:17:32 +0100

    Find all paragraphes of a story DAO

--- src/main/java/dao/ParagrapheDAO.java
+++ src/main/java/dao/ParagrapheDAO.java
@@ -1,9 +1,12 @@
 package dao;
 
+import java.util.List;
+
 import models.Paragraphe;
 
 public interface ParagrapheDAO {
     long saveParagraphe(Paragraphe paragraphe);
     Paragraphe findParagraphe(long story_id, long paragraphe_id);
     void updateParagraphe(Paragraphe paragraphe);
+    List<Paragraphe> findAllStoryParagraphes(long story_id);
 }
--- src/main/java/dao/ParagrapheDAOimpl.java
+++ src/main/java/dao/ParagrapheDAOimpl.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,7 +22,7 @@ public class ParagrapheDAOimpl implements ParagrapheDAO {
     private final static String SQL_FIND_PARAGRAPHE = "SELECT * FROM \"Paragraphe\" WHERE \"story_id\"=? AND \"para_id\"=?";
     private final static String SQL_INSERT_PARAGRAPHE = "INSERT INTO \"Paragraphe\" (\"story_id\", \"content\", \"is_final\") VALUES (?, ?, ?)";
     private final static String SQL_UPDATE_PARAGRAPHE = "UPDATE \"Paragraphe\" SET \"story_id\"=?, \"content\"=?, \"is_final\"=? WHERE \"para_id\"=?";
-
+    private final static String SQL_FIND_ALL_STORY_PARAGRAPHES = "SELECT * FROM \"Paragraphe\" WHERE \"story_id\"=?";;
 
     private final static String SQL_GET_PARAGRAPHE_ID = "SELECT \"PARAGRAPHE_PARA_ID_SEQ\".currval FROM DUAL";
 
@@ -78,18 +81,41 @@ public class ParagrapheDAOimpl implements ParagrapheDAO {
         }
     }
 
+    @Override
+    public List<Paragraphe> findAllStoryParagraphes(long story_id) {
+        List<Paragraphe> stories = new ArrayList<Paragraphe>();
+        try (Connection connection = DatabaseManager.getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(SQL_FIND_ALL_STORY_PARAGRAPHES)) {
+
+            Paragraphe paragraphe = null;
+            while (resultSet.next()) {
+                paragraphe = getParagraphe(resultSet);
+                stories.add(paragraphe);
+            }
+        } catch (SQLException e) {
+            LOG.error("Failed querying story paragraphes", e);
+        }
+
+        return stories;
+    }
+
     private Paragraphe getParagraphe(PreparedStatement preparedStatement) throws SQLException {
         Paragraphe paragraphe = null;
 
         ResultSet resultSet = preparedStatement.executeQuery();
         if (resultSet.next()) {
-            paragraphe = Paragraphe.builder().id(resultSet.getLong(DatabaseFields.PARAGRAPHE_ID))
-                    .story_id(resultSet.getLong(DatabaseFields.STORY_ID))
-                    .content(resultSet.getString(DatabaseFields.PARAGRAPHE_CONTENT))
-                    .is_final((resultSet.getInt(DatabaseFields.PARAGRAPHE_IS_FINAL) == 1)).build();
+            paragraphe = getParagraphe(resultSet);
         }
         resultSet.close();
 
         return paragraphe;
+    }
+
+    private Paragraphe getParagraphe(ResultSet resultSet) throws SQLException {
+        return Paragraphe.builder().id(resultSet.getLong(DatabaseFields.PARAGRAPHE_ID))
+                .story_id(resultSet.getLong(DatabaseFields.STORY_ID))
+                .content(resultSet.getString(DatabaseFields.PARAGRAPHE_CONTENT))
+                .is_final((resultSet.getInt(DatabaseFields.PARAGRAPHE_IS_FINAL) == 1)).build();
     }
 }
